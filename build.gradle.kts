b0061845cc2a7881467981683c000c93d51abfe9
Arkadiy <mefrreex@gmail.com>
2025-07-13 00:30:41 +0400

    feat: maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    `maven-publish`
     id("java")
 }
 
@@ -10,7 +11,7 @@ java {
 }
 
 allprojects {
-    group = "com.luminia"
+    group = "com.luminiadev"
     version = "1.0.6-SNAPSHOT"
 }
 
@@ -28,5 +29,26 @@ subprojects {
         api("org.jetbrains:annotations:26.0.2")
         api("org.projectlombok:lombok:1.18.36")
         annotationProcessor("org.projectlombok:lombok:1.18.36")
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = project.group.toString()
+                artifactId = project.name
+                version = project.version.toString()
+                from(components["java"])
+            }
+        }
+        repositories {
+            maven {
+                name = "luminiadev"
+                url = uri("https://repo.luminiadev.com/snapshots")
+                credentials {
+                    username = System.getenv("MAVEN_USERNAME")
+                    password = System.getenv("MAVEN_PASSWORD")
+                }
+            }
+        }
     }
 }
